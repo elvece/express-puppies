5b60d770bb84a616143d708fd272a2b7cb21c163
elvece <lvcifferello@gmail.com>
2015-08-24 13:25:34 -0600

    added models folder

---
+++ server/models/puppies.js
@@ -0,0 +1,9 @@
+//models folder:
+//data structure
+//wahtever we need to create a new instance of resource
+
+function Puppy(puppyID, puppyName, puppyAge){
+  this.puppyID = puppyID;
+  this.puppyName = puppyName;
+  this.puppyAge = puppyAge;
+}
