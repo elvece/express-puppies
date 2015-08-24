cd6abccbc408614e5e3eaeb2a3db4e93fc376073
elvece <lvcifferello@gmail.com>
2015-08-24 14:26:47 -0600

    added post functionality to puppies

---
+++ readme.md
@@ -0,0 +1,34 @@
+## Intro to Express and CRUD ... with puppies!
+
+***This repo contains my solution to the following exercise:***
+
+![](http://www.unitedwaynorman.org/sites/unitedwaynorman.org/files/cutepuppy.jpg)
+
+### Background
+
+Your task is to build an application that allows the user to see a list of puppies (in JSON) as well as add and see individual puppies. You should use an array of objects to create and display puppies, where each object represents a puppy. Each puppy should have an id which increments, a name, and an age.
+
+## Getting started
+
+### Quick start
+
+```sh
+$ mkdir express-puppies
+$ cd express-puppies
+$ yo galvanize-express
+$ npm install
+$ nodemon
+```
+
+### What's next?
+
+1. CRUD/REST Explanation
+1. Setup/Test each route:
+  - GET all puppies - `/puppies/`
+  - GET single puppy - `/puppy/:id`
+  - POST single puppy - `/puppies/`
+  - PUT single puppy - `/puppy/:id`
+  - DELETE single puppy - `/puppy/:id`
+1. Add a route, `/`, to display a form for adding a new puppy
+1. Refactor to add Mongo
+1. Add unit tests
--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -3,11 +3,49 @@ var router = express.Router();
 var Puppy = require('../models/puppies');//don't need .js as in puppies.js
 var tempPuppyArray = [];
 
-var newPuppy = new Puppy(1, 'bart', 20);
+var bartPuppy = new Puppy(1, 'Bart', 20);
+var johnPuppy = new Puppy(2, 'John', 10);
+var danPuppy = new Puppy(3, 'Dan', 4);
+
+tempPuppyArray.push(bartPuppy, johnPuppy, danPuppy);
 
 router.get('/puppies', function(req, res, next) {
-  console.log(newPuppy);
-  // res.json({});
+  res.json(tempPuppyArray);
+});
+
+router.get('/puppy/:id', function(req, res, next) {
+  var pup = tempPuppyArray.filter(function(puppy){
+    return puppy.puppyID === +req.params.id;
+  });
+  if (pup.length > 0){
+    res.json(pup[0]);
+  } else {
+    res.json("Puppy does not exist here");
+  }
+});
+
+router.post('/puppies', function(req, res, next){
+  //does the puppy already exist
+  var pup = tempPuppyArray.filter(function(puppy){
+    return puppy.puppyID === +req.body.puppyID;
+  });
+  console.log(pup);
+  //if yes,throw error
+  if (pup.length > 0){
+    res.json({message: "Puppy already exists!"});
+  } else { //if no, return success
+    var newPostPuppy = new Puppy(+req.body.puppyID, req.body.puppyName, +req.body.puppyAge);
+    tempPuppyArray.push(newPostPuppy);
+    res.json({
+      message:'success',
+      puppy:newPostPuppy
+    });
+  }
+  console.log(tempPuppyArray);
 });
 
 module.exports = router;
+
+//res.json does not exit out
+//parseInt = floors num ??
+//parseFloat = floats num to whole num?
