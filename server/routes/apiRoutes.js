49dd59bb75c33911f85a2eff2d5944f52a8357cc
elvece <lvcifferello@gmail.com>
2015-08-24 15:16:18 -0600

    added put and delete routes for puppies

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -20,7 +20,7 @@ router.get('/puppy/:id', function(req, res, next) {
   if (pup.length > 0){
     res.json(pup[0]);
   } else {
-    res.json("Puppy does not exist here");
+    res.json({message: "Puppy does not exist here"});
   }
 });
 
@@ -44,8 +44,62 @@ router.post('/puppies', function(req, res, next){
   console.log(tempPuppyArray);
 });
 
+router.put('/puppy/:id', function(req, res, next){
+  //add validation for chekcking if the puppyAge is an integer
+    //if exists, and not an integer, throw an error
+
+  var pup = tempPuppyArray.filter(function(puppy){
+    return puppy.puppyID === +req.params.id;
+  });
+
+  if (pup.length > 0){
+    //grab object from array
+    for (var i = 0; i < tempPuppyArray.length; i++) {
+      if (tempPuppyArray[i].puppyID === +req.params.id){
+        //looping through keys in object
+        for (key in req.body) {//these keys are strings irl
+          if (key === 'puppyName'){
+            tempPuppyArray[i].puppyName = req.body.puppyName;
+          } else if (key === 'puppyAge') {
+            tempPuppyArray[i].puppyAge = req.body.puppyAge;
+          }
+        }
+      }
+    }
+    res.send(tempPuppyArray);
+    //update specific keys
+    //push back into the array
+
+  } else {
+    res.json("Puppy does not exist here");
+  }
+});
+
+router.delete('/puppy/:id', function(req, res, next){
+  var pup = tempPuppyArray.filter(function(puppy){
+    return puppy.puppyID === +req.params.id;
+  });
+  if (pup.length > 0){
+    for (var i = 0; i < tempPuppyArray.length; i++) {
+      if (tempPuppyArray[i].puppyID === +req.params.id){
+        var tempPuppy = tempPuppyArray.splice(i, 1);
+        res.send({
+          message: 'That puppy is gone!',
+          puppy: tempPuppy
+        });
+      }
+    }
+  } else {
+    res.json("Puppy does not exist here");
+  }
+});
+
+
+
 module.exports = router;
 
+//loop through objects with for, in
 //res.json does not exit out
 //parseInt = floors num ??
 //parseFloat = floats num to whole num?
+
